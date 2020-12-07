74d95a0b04af44024a01e4fabd8c7c0688a2a03c
jayranpariya <58929152+jayranpariya@users.noreply.github.com>
2020-12-07 12:20:08 +0530

    https://github.com/jayranpariya/cpp/tree/master/sort

---
+++ sort/DNF_sort.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+#include "bits/stdc++.h"
+
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int l = 0;
+    int m = 0;
+    int h = n - 1;
+
+    while (m <= h)
+    {
+
+        if (a[m] == 0)
+        {
+            int temp;
+            // temp = a[l];
+            // a[l] = a[m];
+            // a[m] = temp;
+            swap(a[l],a[m]);
+            l++;
+            m++;
+        }
+        else if (a[m] == 1)
+        {
+            m++;
+        }
+        else
+        {
+            int temp1;
+            // temp1 = a[m];
+            // a[m] = a[h];
+            // a[h] = temp1;
+            swap(a[h],a[m]);
+            h--;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
